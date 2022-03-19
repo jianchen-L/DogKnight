5403d34d0065711af92a1b6b77ec464c5b4735b9
jianchen <zengjianchengchn@foxmail.com>
2022-03-19 22:16:33 +0800

    设置石头人

--- Assets/Scripts/Characters/Enemy/Golem.cs
+++ Assets/Scripts/Characters/Enemy/Golem.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Grunt : EnemyController
+public class Golem : EnemyController
 {
     [Header("Skill")]
     public float kickForce;
 
     public void KickOff()
     {
-        if (attackTarget != null)
+        if (attackTarget != null || transform.IsFacingTarget(attackTarget.transform))
         {
-            transform.LookAt(attackTarget.transform);
+            var targetStats = attackTarget.GetComponent<CharacterStats>();
             Vector3 direction = attackTarget.transform.position - transform.position;
             direction.Normalize();
             attackTarget.GetComponent<NavMeshAgent>().isStopped = true;
             attackTarget.GetComponent<NavMeshAgent>().velocity = direction * kickForce;
             attackTarget.GetComponent<Animator>().SetTrigger("Dizzy");
+            targetStats.TakeDamage(characterStats, targetStats);
         }
     }
 }
